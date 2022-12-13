db2c4b25dbb9d252de4380697d587172f1974f67
Sompoon Lind <72384939+SompoonLind@users.noreply.github.com>
2022-12-13 15:44:06 +0100

    boxplot, median, mean, mannwhitney, ttest

--- sem5Data.R
+++ sem5Data.R
@@ -2,7 +2,6 @@
 # Alt + Enter to run line
 # datasetOGF is Our Guardian Feature
 # Both ID & ID_OGF go from 1-15 so the y-axis can draw 1-15.
-options(stringsAsFactors=FALSE)
 
 library(ggplot2)
 
@@ -13,46 +12,144 @@ median(dataset$Age_OGF) # 24
 
 # NASA RAW of Standard Guardian Feature
 # How mentally demanding was the task?
+# Histogram
 hGramMD <- ggplot(dataset, aes(x=dataset$NASA_MD)) 
-hGram + geom_histogram(binwidth = 1, colour = "black", fill="steelblue2") +
-  ggtitle("Histogram for the mental demand required for use the standard guardian feature") +
-  labs(x= "Scale from 1-20") +
+hGramMD + geom_histogram(binwidth = 1, colour = "black", fill="steelblue2") +
+  ggtitle("Histogram for 'How mentally demanding was the task?' of the Standard Guardian Feature") +
+  labs(x= "1 - Low, 20 - High") +
   coord_cartesian(xlim = c(0,20))
 
-# How physically demanding was the task?
-hGramPD <- ggplot(dataset, aes(x=dataset$NASA_PD))
-hGram + geom_histogram(binwidth = 1, colour = "black", fill="steelblue2") +
-  ggtitle("Histogram for the physical demand required for use the standard guardian feature") +
-  labs(x= "Scale from 1-20") +
+hGramMD <- ggplot(dataset, aes(x=dataset$NASA_MD_OGF)) 
+hGramMD + geom_histogram(binwidth = 1, colour = "black", fill="red") +
+  ggtitle("Histogram for 'How mentally demanding was the task?' of Our Guardian Feature") +
+  labs(x= "1 - Low, 20 - High") +
   coord_cartesian(xlim = c(0,20))
+# Boxplot
+par(mar = c(5, 4, 4, 2) + 0.1)
+boxplot(dataset$NASA_MD, dataset$NASA_MD_OGF,
+        main = "How mentally demanding was the task?",
+        names = c("Standard Guardian Feature", "Our Guardian Feature"),
+        horizontal = FALSE,
+        col = "red",
+        notch = FALSE,
+        ylim = c(0, 20)
+)
+mean(dataset$NASA_MD)
+mean(dataset$NASA_MD_OGF)
+median(dataset$NASA_MD)
+median(dataset$NASA_MD_OGF)
+
+# How physically demanding was the task?
+# Histogram
+hGramPD <- ggplot(dataset, aes(x=dataset$NASA_PD))
+hGramPD + geom_histogram(binwidth = 1, colour = "black", fill="steelblue2") +
+  ggtitle("Histogram for 'How physically demanding was the task?' of the Standard Guardian Feature") +
+  labs(x= "1 - Low, 20 - High") +
+  coord_cartesian(xlim = c(0,20))
+
+# Boxplot
+par(mar = c(5, 4, 4, 2) + 0.1)
+boxplot(dataset$NASA_PD, dataset$NASA_PD_OGF,
+        main = "How physically demanding was the task?",
+        names = c("Standard Guardian Feature", "Our Guardian Feature"),
+        horizontal = FALSE,
+        col = "red",
+        notch = FALSE,
+        ylim = c(0, 20)
+)
+mean(dataset$NASA_PD)
+mean(dataset$NASA_PD_OGF)
+median(dataset$NASA_PD)
+median(dataset$NASA_PD_OGF)
 
 # How hurried or rushed was the pace of the task?
 hGramRUSH <- ggplot(dataset, aes(x=dataset$NASA_RUSH))
-hGram + geom_histogram(binwidth = 1, colour = "black", fill="steelblue2") +
-  ggtitle("Histogram for how rushed the users felt during the test") +
-  labs(x= "Scale from 1-20") +
+hGramRUSH + geom_histogram(binwidth = 1, colour = "black", fill="steelblue2") +
+  ggtitle("Histogram for 'How hurried or rushed was the pace of the task?' of the Standard Guardian Test") +
+  labs(x= "1 - Low, 20 - High") +
   coord_cartesian(xlim = c(0,20))
 
+# Boxplot
+par(mar = c(5, 4, 4, 2) + 0.1)
+boxplot(dataset$NASA_RUSH, dataset$NASA_RUSH_OGF,
+        main = "How hurried or rushed was the pace of the task?",
+        names = c("Standard Guardian Feature", "Our Guardian Feature"),
+        horizontal = FALSE,
+        col = "red",
+        notch = FALSE,
+        ylim = c(0, 20)
+)
+mean(dataset$NASA_RUSH)
+mean(dataset$NASA_RUSH_OGF)
+median(dataset$NASA_RUSH)
+median(dataset$NASA_RUSH_OGF)
 # How hard did you have to work to accomplish your level of performance?
+# Histogram
 hGramLOP <- ggplot(dataset, aes(x=dataset$NASA_PERFORMANCEDEMAND))
 hGramLOP + geom_histogram(binwidth = 1, colour = "black", fill="steelblue2") +
-  ggtitle("Histogram for the users subjective performance level") +
-  labs(x= "Scale from 1-20") +
+  ggtitle("Histogram for 'How hard did you have to work to accomplish your level of performance?' of the Standard Guardian Feature") +
+  labs(x= "1 - Very Low, 20 - Very High") +
   coord_cartesian(xlim = c(0,20))
+
+# Boxplot
+par(mar = c(5, 4, 4, 2) + 0.1)
+boxplot(dataset$NASA_PERFORMANCEDEMAND, dataset$NASA_PERFORMANCEDEMAND_OGF,
+        main = "How hard did you have to work to accomplish your level of performance?",
+        names = c("Standard Guardian Feature", "Our Guardian Feature"),
+        horizontal = FALSE,
+        col = "red",
+        notch = FALSE,
+        ylim = c(0, 20)
+)
+mean(dataset$NASA_PERFORMANCEDEMAND)
+mean(dataset$NASA_PERFORMANCEDEMAND_OGF)
+median(dataset$NASA_PERFORMANCEDEMAND)
+median(dataset$NASA_PERFORMANCEDEMAND_OGF)
 
 # How successful were you in accomplishing what you were asked to do?
-hGramLOS <- ggplot(dataset, aes(x=dataset$NASA_SUBJECTIVE_SUCCES))
-hGram + geom_histogram(binwidth = 1, colour = "black", fill="steelblue2") +
-  ggtitle("Histogram for participants subjective level of success") +
-  labs(x= "Scale from 1-20") +
+hGramLOS <- ggplot(dataset, aes(x=dataset$NASA_SUBJECTIVE_SUCCES_OGF))
+hGramLOS + geom_histogram(binwidth = 1, colour = "black", fill="steelblue2") +
+  ggtitle("Histogram for 'How successful were you in accomplishing what you were asked to do?' of Our Guardian Feature") +
+  labs(x= "1 - Low, 20 - High") +
   coord_cartesian(xlim = c(0,20))
 
+# Boxplot
+par(mar = c(5, 4, 4, 2) + 0.1)
+boxplot(dataset$NASA_SUBJECTIVE_SUCCES, dataset$NASA_SUBJECTIVE_SUCCES_OGF,
+        main = "How successful were you in accomplishing what you were asked to do?",
+        names = c("Standard Guardian Feature", "Our Guardian Feature"),
+        horizontal = FALSE,
+        col = "red",
+        notch = FALSE,
+        ylim = c(0, 20)
+)
+mean(dataset$NASA_SUBJECTIVE_SUCCES)
+mean(dataset$NASA_SUBJECTIVE_SUCCES_OGF)
+median(dataset$NASA_SUBJECTIVE_SUCCES)
+median(dataset$NASA_SUBJECTIVE_SUCCES_OGF)
 # How insecure, discouraged, irritated, stressed, and annoyed were you?
 hGramIDISA <- ggplot(dataset, aes(x=dataset$NASA_IDISA))
-hGram + geom_histogram(binwidth = 1, colour = "black", fill="steelblue2") +
-  ggtitle("Histogram for how insecure, discouraged, irritated, stressed and annoyed the participant felt during the test") +
-  labs(x= "Scale from 1-20") +
+hGramIDISA + geom_histogram(binwidth = 1, colour = "black", fill="steelblue2") +
+  ggtitle("Histogram for 'How insecure, discouraged, irritated, stressed and annoyed were you?' of the Standard Guardian") +
+  labs(x= "1 - Low, 20 - High") +
   coord_cartesian(xlim = c(0,20))
+
+# Boxplot
+par(mar = c(5, 4, 4, 2) + 0.1)
+boxplot(dataset$NASA_IDISA, dataset$NASA_IDISA_OGF,
+        main = "How insecure, discouraged, irritated, stressed, and annoyed were you?",
+        names = c("Standard Guardian Feature", "Our Guardian Feature"),
+        horizontal = FALSE,
+        col = "red",
+        notch = FALSE,
+        ylim = c(0, 20)
+)
+mean(dataset$NASA_IDISA)
+mean(dataset$NASA_IDISA_OGF)
+median(dataset$NASA_IDISA)
+median(dataset$NASA_IDISA_OGF)
+library(moments)
+# Kurtosis & Skewness
 
 # System Usability Scale
 # https://github.com/ekapros/sus.compute.R
@@ -63,6 +160,81 @@ hGram + geom_histogram(binwidth = 1, colour = "black", fill="steelblue2") +
 # 51 – 68     Poor Score
 # <51         Awful Score
 
+
 # Husk at tjekke kurtosis og skewness og den slags
 # Måske er det muligt at få mere normalt fordelt data ved LOG el. squareroot af vores data
 # Graf med boxplots af NASA Raw spørgsmål???
+
+# Mann Whitney Test & T-Test
+wilcox.test(dataset$NASA_MD, dataset$NASA_MD_OGF)
+# w = 41.5, p = 0.003353
+wilcox.test(dataset$NASA_PD, dataset$NASA_PD_OGF) 
+# w = 83, p = 0.2229
+wilcox.test(dataset$NASA_RUSH, dataset$NASA_RUSH_OGF) 
+# w = 91.5, p = 0.3908
+wilcox.test(dataset$NASA_PERFORMANCEDEMAND, dataset$NASA_PERFORMANCEDEMAND_OGF) 
+# w = 39.5, p = 0.002539
+wilcox.test(dataset$NASA_SUBJECTIVE_SUCCES, dataset$NASA_SUBJECTIVE_SUCCES_OGF) 
+# 118.8, p = 0.8185
+wilcox.test(dataset$NASA_IDISA, dataset$NASA_IDISA_OGF) 
+# w = 55, p = 0.01775
+
+t.test(dataset$NASA_MD, dataset$NASA_MD_OGF)
+# data:  dataset$NASA_MD and dataset$NASA_MD_OGF
+# t = -3.4172, df = 27.02, p-value = 0.002018
+# alternative hypothesis: true difference in means is not equal to 0
+# 95 percent confidence interval:
+#   -9.069029 -2.264304
+# sample estimates:
+#   mean of x mean of y 
+# 8.466667 14.133333 
+
+t.test(dataset$NASA_PD, dataset$NASA_PD_OGF)
+# data:  dataset$NASA_PD and dataset$NASA_PD_OGF
+# t = -1.0679, df = 27.862, p-value = 0.2947
+# alternative hypothesis: true difference in means is not equal to 0
+# 95 percent confidence interval:
+#   -4.864179  1.530846
+# sample estimates:
+#   mean of x mean of y 
+# 4.466667  6.133333 
+
+t.test(dataset$NASA_RUSH, dataset$NASA_RUSH_OGF)
+# data:  dataset$NASA_RUSH and dataset$NASA_RUSH_OGF
+# t = -0.93883, df = 27.499, p-value = 0.356
+# alternative hypothesis: true difference in means is not equal to 0
+# 95 percent confidence interval:
+#   -4.032630  1.499297
+# sample estimates:
+#   mean of x mean of y 
+# 4.666667  5.933333 
+
+t.test(dataset$NASA_PERFORMANCEDEMAND, dataset$NASA_PERFORMANCEDEMAND_OGF)
+# data:  dataset$NASA_PERFORMANCEDEMAND and dataset$NASA_PERFORMANCEDEMAND_OGF
+# t = -3.7849, df = 21.01, p-value = 0.001085
+# alternative hypothesis: true difference in means is not equal to 0
+# 95 percent confidence interval:
+#   -10.123011  -2.943655
+# sample estimates:
+#   mean of x mean of y 
+#5.80000  12.33333 
+
+t.test(dataset$NASA_SUBJECTIVE_SUCCES, dataset$NASA_SUBJECTIVE_SUCCES_OGF)
+# data:  dataset$NASA_SUBJECTIVE_SUCCES and dataset$NASA_SUBJECTIVE_SUCCES_OGF
+# t = 0.99897, df = 18.148, p-value = 0.3309
+# alternative hypothesis: true difference in means is not equal to 0
+# 95 percent confidence interval:
+#   -1.836416  5.169749
+#  estimates:
+#   mean of x mean of y 
+# 14.26667  12.60000 
+
+t.test(dataset$NASA_IDISA, dataset$NASA_IDISA_OGF)
+# :  dataset$NASA_IDISA and dataset$NASA_IDISA_OGF
+# t = -2.5509, df = 27.766, p-value = 0.01655
+# alternative hypothesis: true difference in means is not equal to 0
+# 95 percent confidence interval:
+#   -9.0165595 -0.9834405
+# sample estimates:
+#   mean of x mean of y 
+# 7.2      12.2 
